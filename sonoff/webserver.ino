0c483868b587fb8342b4314ca92c42972ce120c2
geitde <geit@geit.de>
2018-04-20 23:24:44 +0200

    fix build

--- sonoff/webserver.ino
+++ sonoff/webserver.ino
@@ -961,7 +961,7 @@ void HandleBackupConfiguration()
   WebServer->setContentLength(sizeof(buffer));
 
   char attachment[100];
-  snprintf_P(attachment, sizeof(attachment), PSTR("attachment; filename=Config_%s_%s.dmp"), NoAlNumToUnderscore(hostname, Settings.hostname), my_version);
+  snprintf_P(attachment, sizeof(attachment), PSTR("attachment; filename=Config_%s_%s.dmp"), Settings.hostname, my_version);
   WebServer->sendHeader(F("Content-Disposition"), attachment);
   WebServer->send(200, FPSTR(HDR_CTYPE_STREAM), "");
   memcpy(buffer, &Settings, sizeof(buffer));
